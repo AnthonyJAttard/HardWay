92c9aee7078b20768c8296a876ab02f6671d9471
Anthony Attard <attaant@bigpond.net.au>
2018-02-28 17:20:11 +1100

    Commit on 28-02-2018 at home

--- Ex23.py
+++ Ex23.py
@@ -3,17 +3,17 @@ import sys
 script, encoding, error = sys.argv
 
 
-def main(language_file, encoding, errors):
-    line = language_file.readLine()
+def main(language_file, encodings, errors):
+    line = language_file.readline()
     if line:
-        print_line(line, encoding, errors)
-        return main(language_file, encoding, errors)
+        print_line(line, encodings, errors)
+        return main(language_file, encodings, errors)
 
 
-def print_line(line, encoding, errors):
+def print_line(line, encodings, errors):
     next_lang = line.strip()
-    raw_bytes = next_lang.encode(encoding, errors=errors)
-    cooked_string = raw_bytes.decode(encoding, errors=errors)
+    raw_bytes = next_lang.encode(encodings, errors=errors)
+    cooked_string = raw_bytes.decode(encodings, errors=errors)
 
     print(raw_bytes, "<====>", cooked_string)
 
